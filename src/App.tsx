0c118ec53c8526eebc64e76d0b028bfb093d6d07
Touseef <ahmedjamiil509@gmail.com>
2023-10-19 01:30:59 +0500

    Changed

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
       <div className=" bg-slate-600 w-screen h-screen flex justify-center items-center ">
-        <div className="w-1/5 h-auto flex flex-col justify-center  bg-slate-400 p-3 rounded-md ">
+        <div className="w-1/5 h-auto flex flex-col justify-center  bg-slate-400 p-3 pt-10 pb-10 rounded-md ">
           <h2 className="text-lg font-bold text-blue-700">Login Form</h2>
           <div className="  h-14 w-1/2 p-2 flex flex-col ml-16 mt-3 ">
             <img
@@ -27,7 +27,7 @@ function App() {
           <div className=" flex flex-col items-start m-1">
             <label className="text-black-500">UserName</label>
             <input
-              className="border-solid border-2 border-gray-500 rounded-md px-1"
+              className="border-solid border-2 border-gray-500 rounded-md px-1 w-full"
               type="text"
               placeholder="Your Name"
               required
@@ -37,14 +37,18 @@ function App() {
             />
             <label className="text-black-500">Password</label>
             <input
-              className="border-2 border-gray-500 rounded-md px-1"
+              className="border-2 border-gray-500 rounded-md px-1 w-full"
               type="password"
               required
               onChange={(e) => {
                 onPasswordChange(e.target.value);
               }}
             />
+            <h2 className=" font-normal text-blue-700 cursor-pointer">
+              Forget Password*
+            </h2>
           </div>
+
           <div className="flex w-full  flex-col items-center my-2 ">
             <button className="font-bold text-blue-700 ">Log In</button>
           </div>
